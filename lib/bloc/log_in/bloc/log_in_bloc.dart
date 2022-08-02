15d6b931e100de55f381ec4de63db84e2d83cdb6
Quoc Anh <52539949+qabk@users.noreply.github.com>
2022-08-02 07:06:39 +0700

    fix some bug

--- lib/bloc/log_in/bloc/log_in_bloc.dart
+++ lib/bloc/log_in/bloc/log_in_bloc.dart
@@ -4,13 +4,16 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:yugi_oh_cards/bloc/log_in/bloc/log_in_state.dart';
+import 'package:yugi_oh_cards/commons/constant.dart';
 import 'package:yugi_oh_cards/services/firebase_auth_service.dart';
 import 'package:yugi_oh_cards/services/firebase_firestore_service.dart';
+
+import '../../../models/user_model.dart';
 
 part 'log_in_event.dart';
 
 class LogInBloc extends Bloc<LogInEvent, LogInState> {
-  LogInBloc() : super(const LogInState.initState()) {
+  LogInBloc() : super(LogInState.initState()) {
     on<LogInEvent>((event, emit) {});
     on<LogInSubmit>(_onSubmit);
     on<LogInChangeInfo>(_onChangeInfo);
@@ -26,14 +29,20 @@ class LogInBloc extends Bloc<LogInEvent, LogInState> {
       String result =
           await FirebaseAuthService().emailSignIn(event.email, event.password);
       if (result != "some error happend") {
-        FireStoreService().getUserByUid(result);
-        emit(LogInState(message: result, status: LogInStatus.success));
+        MyUser user = FireStoreService().getUserByUid(result);
+        emit(LogInState(
+            message: result, status: LogInStatus.success, myUser: user));
       } else {
-        emit(const LogInState(
-            message: "some error happend", status: LogInStatus.error));
+        emit(LogInState(
+            message: "some error happend",
+            status: LogInStatus.error,
+            myUser: Constant().errorUser));
       }
     } catch (e) {
-      emit(LogInState(message: e.toString(), status: LogInStatus.error));
+      emit(LogInState(
+          message: e.toString(),
+          status: LogInStatus.error,
+          myUser: Constant().errorUser));
     }
   }
 
@@ -41,6 +50,9 @@ class LogInBloc extends Bloc<LogInEvent, LogInState> {
       LogInChangeInfo event, Emitter<LogInState> emit) {
     if (state.status == LogInStatus.success) {
       FireStoreService().updateInfo(state.message!, event.field, event.data);
+       MyUser user = FireStoreService().getUserByUid(state.message!);
+        emit(LogInState(
+            message:state.message!, status: LogInStatus.success, myUser: user));
     }
   }
 }
--- lib/bloc/log_in/bloc/log_in_state.dart
+++ lib/bloc/log_in/bloc/log_in_state.dart
@@ -1,17 +1,24 @@
 import 'package:equatable/equatable.dart';
+import 'package:yugi_oh_cards/commons/constant.dart';
+import 'package:yugi_oh_cards/models/user_model.dart';
 
 enum LogInStatus { init, error, success }
 
 class LogInState extends Equatable {
   final LogInStatus? status;
   final String? message;
+  final MyUser? myUser;
   const LogInState({
     this.status,
     this.message,
+    this.myUser,
   });
-  const LogInState.initState() : this(status: LogInStatus.init);
+  LogInState.initState()
+      : this(
+            message: "init",
+            status: LogInStatus.init,
+            myUser: Constant().errorUser);
   LogInState copyWith({
-    
     String? message,
     LogInStatus? logInStatus,
   }) {
@@ -21,9 +28,14 @@ class LogInState extends Equatable {
     );
   }
 
-  const LogInState.error( String mess) : this(message: mess,status: LogInStatus.error);
-  const LogInState.success(String uid) : this(message: uid,status: LogInStatus.success);
+  LogInState.error(String mess)
+      : this(
+            message: mess,
+            status: LogInStatus.error,
+            myUser: Constant().errorUser);
+  const LogInState.success(String uid, MyUser myUser)
+      : this(message: uid, myUser: myUser, status: LogInStatus.success);
 
   @override
-  List<Object?> get props => [ message,status];
+  List<Object?> get props => [message, status,myUser];
 }
